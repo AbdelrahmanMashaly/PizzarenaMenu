971e88f10b1052db76ddbdf948dd9e60f8bb0bcd
AbdelrahmanMashaly <abdelrahmanmashaly24@outlook.com>
2022-12-14 02:45:17 +0200

    PizzaBuilder Page + Checkout page

--- PizzarenaMenu/Pages/Checkout/Checkout.cshtml.cs
+++ PizzarenaMenu/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzarenaMenu.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImgTitle { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(ImgTitle))
+            {
+                ImgTitle = "Create";
+            }
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
         }
     }
 }
