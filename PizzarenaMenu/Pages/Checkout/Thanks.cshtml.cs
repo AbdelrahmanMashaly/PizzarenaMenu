58d677285ad723ffd5bac4f36b31547cdd7fa33f
AbdelrahmanMashaly <abdelrahmanmashaly24@outlook.com>
2022-12-14 02:14:47 +0200

    Add project files.

---
+++ PizzarenaMenu/Pages/Checkout/Thanks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzarenaMenu.Pages.Checkout
+{
+    public class ThanksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
